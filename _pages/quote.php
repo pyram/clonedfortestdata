f649b494df1e500880d4a63471671de975e98be4
michael pyra <pyram@tlen.pl>
2016-11-22 00:15:02 +0100

    Files generated by jekyll build site using code from develop branch

--- _pages/quote.php
+++
@@ -1,6 +0,0 @@
----
-layout: default
-title: Some page title
-permalink: quote.php
----
-{% raw %}<?php echo ("parsed"); ?>{% endraw %}
---
+++ quote.php
@@ -0,0 +1,162 @@
+
+  
+
+
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl" dir="ltr" class="no-js">
+  <head>
+    
+  
+
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+
+
+	<meta name="keywords" content="Rightsourcing, software development blog, software outsourcing, outsourcing to Poland, outsourcing to India" />
+	<meta name="rights" content="Neubloc" />
+	<meta name="description" content="Industry leader in Rightsourcing software development." />
+	<meta name="generator" content="Joomla! - Open Source Content Management" />
+	<link href="component/search/indexbe1d.html?Itemid=109&amp;format=opensearch" rel="search" title="Search Neubloc" type="application/opensearchdescription+xml" />
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,500" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="https://neubloc-poland.github.io/www-neubloc-pl/assets/css/style.min.css" />
+	<link rel="shortcut icon" href="https://neubloc-poland.github.io/www-neubloc-pl/images/favicon.ico" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- start custom head snippets -->
+
+<!-- insert favicons. use http://realfavicongenerator.net/ -->
+
+<!-- end custom head snippets -->
+
+  
+
+
+
+
+  
+
+
+
+  
+
+
+
+  
+
+
+<title>Some page title - Neubloc - www.neubloc.pl</title>
+  </head>
+  <body>
+    <div id="page-wrap">
+      <header>   
+      <!--[if lt IE 9]>
+<div class="notice--danger align-center" style="margin: 0;">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</div>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+      
+  
+
+
+
+<div class="header-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <a href="index.html" class="logo"><img alt="Neubloc: Rightsource Software Design and Development" src="https://neubloc-poland.github.io/www-neubloc-pl/images/logo.png" /></a>
+            </div>
+            <div class="col-sm-4">
+                <div class="search">
+                    <form action="http://www.neubloc.com/" method="post" class="top-search" role="search">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search our website" name="searchword" />
+                            <span class="input-group-btn"><button class="btn btn-default" type="submit" title="Search"><i class="nico-search"></i><span class="hidden">Search</span></button>
+                            </span>
+                        </div>
+                        <input type="hidden" name="task" value="search" />
+                        <input type="hidden" name="option" value="com_search" />
+                        <input type="hidden" name="Itemid" value="101" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="main-nav">
+    <nav class="navbar" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+                    <!-- /quote.php -->
+                    
+                    
+                    
+            <div class="collapse navbar-collapse" id="main-nav-collapse">            
+                <ul class="nav menu navbar-nav">
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-1 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/">Strona Główna</a></li>
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-2 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/about-us/">O Nas</a></li>
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-3 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/services/">Usługi</a></li>
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-4 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/case-studies/">Studia Przypadków</a></li>
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-5 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/expertise/">Wiedza Specjalistyczna</a></li>
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-6 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/clients/">Klienci</a></li>
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-7 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/blog/">Blog</a></li>
+                    
+                    
+                      
+                    
+                    
+                    <li class="item-8 parent "><a style="padding-right: 15px; padding-left: 15px;" href="/www-neubloc-pl/contact/">Kontakt</a></li>
+                    
+                                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+      </header>
+    <?php echo ("parsed"); ?>
+    </div>
+  </body>
+</html>

366149bd8968e84022629372822f6951d2227e4b
michael pyra <pyram@tlen.pl>
2017-03-24 14:35:17 +0100

    setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
-
   def about
   end
-
-  def clients
-  end
-
-  def community
-  end
-
   def contact
   end
-
   def jobs
   end
-  
+  def clients
+  end
+  def community
+  end
 end

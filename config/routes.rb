2b8000566aaa3443bc5d0fe6199272afac6797e1
michael pyra <pyram@tlen.pl>
2017-03-24 14:44:31 +0100

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/about'
-  get 'home/contact'
   get 'home/clients'
-  get 'home/jobs'
   get 'home/community'
+  get 'home/contact'
+  get 'home/jobs'
+  get 'home/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

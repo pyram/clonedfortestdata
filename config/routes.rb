f562f2213101a2a21fd0f902374d729094210158
kubaleduchowski <leduchowskijakub@gmail.com>
2017-03-29 11:46:27 +0200

    added links to about button

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'home/contact'
   get 'home/jobs'
   get 'home/about'
+  get 'home/why_neubloc'
+  get 'home/our_history'
+  get 'home/management'
+  get 'home/equity_option'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

66c1d62ae49e76f2f11c7b0262b11a204b49b2ba
kubaleduchowski <leduchowskijakub@gmail.com>
2017-03-29 22:31:42 +0200

    added new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/clients'
-  get 'home/community'
-  get 'home/contact'
-  get 'home/jobs'
-  get 'home/about'
-  get 'home/why_neubloc'
-  get 'home/our_history'
-  get 'home/management'
-  get 'home/equity_option'
-  get 'home/our_history'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#index"
 end

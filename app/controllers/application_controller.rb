26e9428eee2186bd62cf225a73ab4784a8e8be58
michael pyra <pyram@tlen.pl>
2017-04-12 22:03:32 +0200

    first steps of scraper
    first steps of scraper
    
    scarpe jobs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def scrape_reddit
-    render text: 'scrape reddit data here'
+  class Entry
+  	def initialize(title, link)
+  		@title = title
+  		@link = link
+  	end
+  	attr_reader :title
+  	attr_reader :link
   end
 end

fa4c4b84bb81a00cbe4b1f33626d376bc900778b
michael pyra <pyram@tlen.pl>
2017-04-12 22:03:32 +0200

    first steps of scraper
    first steps of scraper
    
    scarpe jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def index
+     scrape_reddit
   end
+
+  def scrape_reddit
+    require 'open-uri'
+    doc = Nokogiri::HTML(open("https://www.pracuj.pl/praca/Neubloc;kw"))
+    entries = doc.css('.o-list>li.o-list_item')
+    @entriesArray = []
+     entries.each do |entry|
+       title = entry.css('a.o-list_item_link_name')[0].text
+       link = 'https://pracuj.pl' + entry.css('a.o-list_item_link_name')[0]['href']
+       @entriesArray << Entry.new(title, link)
+     end
+    #  render template: 'pages/scrape_reddit'
+    end
+
 end

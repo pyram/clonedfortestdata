b3ca1a1497c172c8261617d36feadff0e5b48e38
kubaleduchowski <leduchowskijakub@gmail.com>
2017-04-18 12:59:42 +0200

    changed colors button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   class Entry
-  	def initialize(title, link)
+  	def initialize(title, link, locations)
   		@title = title
   		@link = link
+      @locations = locations
   	end
   	attr_reader :title
   	attr_reader :link
+    attr_reader :locations
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,38 @@
 class PagesController < ApplicationController
+  before_filter :require_pracuj_pl_jobs
+  skip_before_filter :only => [:index]
+
   def index
-     scrape_reddit
+    #  scrape_pracuj_pl
   end
 
-  def scrape_reddit
+  def scrape_pracuj_pl
     require 'open-uri'
   # @search_term = 'Neubloc'
   @search_term = 'Neubloc'
+  url_prefix = 'https://pracuj.pl'
   if request.parameters['st']!=NIL
      @search_term = request.parameters['st']
   end
     doc = Nokogiri::HTML(open("https://www.pracuj.pl/praca/" + @search_term + ";kw"))
-    entries = doc.css('.o-list>li.o-list_item')
+    # entries = doc.xpath("//span[@itemprop='title']/..")
+    entries = doc.xpath('//li[@itemtype=\'http://schema.org/JobPosting\']')
     @entriesArray = []
-     entries.each do |entry|
-       title = entry.css('a.o-list_item_link_name')[0].text
-       link = 'https://pracuj.pl' + entry.css('a.o-list_item_link_name')[0]['href']
-       @entriesArray << Entry.new(title, link)
+    entries.each do |entry|
+        # location = ''
+        locationsArray = []
+        entry.xpath('.//a[@itemprop="addressRegion"]').each do |loc|
+          locationsArray << Entry.new(loc.text, url_prefix + loc['href'], [])
+        end
+        title = entry.xpath('.//*[@itemprop="title"]').text
+       link = url_prefix + entry.xpath(".//a[@class='o-list_item_link_name']")[0]['href']
+       @entriesArray << Entry.new(title, link, locationsArray)
      end
-    #  render template: 'pages/scrape_reddit'
-    end
+    #  render template: 'pages/scrape_pracuj_pl'
+  end
 
+    private
+      def require_pracuj_pl_jobs
+        scrape_pracuj_pl
+      end
 end

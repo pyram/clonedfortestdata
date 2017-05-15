a351554ca97283dca1cc49deb1941cd1fd27878b
pyram <pyram@tlen.pl>
2017-05-15 16:52:55 +0200

    rewritten rake task - changes according to: - use underscore in variable names - maximum 5 lines per method - corrected identation - added logger

--- app/services/job_collector.rb
+++ app/services/job_collector.rb
@@ -1,36 +1,25 @@
 # lib/job_collector.rb
 class JobCollector
-
-  # attr_reader :user, :questions, :answers, :errors
-
-  # def initialize(user)
-  #   @user      = user
-  #   @questions = user.questions
-  #   @answers   = user.answers
-  # end
-
   def collect
-  require 'open-uri'
-  @search_term = 'Neubloc' + ";kw"
-  url_prefix = 'https://pracuj.pl'
-  # if request.parameters['st']!=NIL
-  #    @search_term = request.parameters['st'] + ";kw"
-  # end
-    doc = Nokogiri::HTML(open("https://www.pracuj.pl/praca/" + @search_term))
-
-    entries = doc.xpath('//li[@itemtype=\'http://schema.org/JobPosting\']')
+    @url_prefix = 'https://pracuj.pl'
+    require 'open-uri'
     Location.delete_all
     Job.delete_all
-    entries.each do |entry|
-       title = entry.xpath('.//*[@itemprop="title"]').text
-       link = url_prefix + entry.xpath(".//a[@class='o-list_item_link_name']")[0]['href']
-       newjob = Job.new(:name => title, :info =>title)
-       newjob.save()
-       entry.xpath('.//a[@itemprop="addressRegion"]').each do |loc|
-          url_link = url_prefix + loc['href']
-          newjoblocation = Location.new(:name => loc.text, :info =>title, :url => url_link, :job_id =>newjob.id )
-          newjoblocation.save()
-        end
-     end
+    scrape_jobs('https://www.pracuj.pl/praca/Neubloc;kw')
+  end
+
+  def scrape_jobs(target_url)
+    Nokogiri::HTML(open(target_url)).xpath('//li[@itemtype=\'http://schema.org/JobPosting\']').each do |entry|
+      new_job = Job.new(name: entry.xpath('.//*[@itemprop="title"]').text, info: @url_prefix + entry.xpath(".//a[@class='o-list_item_link_name']")[0]['href'])
+      new_job.save
+      scrape_job_locations(entry, new_job)
+    end
+  end
+
+  def scrape_job_locations(job_entry, new_job)
+    job_entry.xpath('.//a[@itemprop="addressRegion"]').each do |loc|
+      new_job_location = Location.new(name: loc.text, info: new_job.name, url: @url_prefix + loc['href'], job_id: new_job.id)
+      new_job_location.save
+    end
   end
 end
--- lib/tasks/site.rake
+++ lib/tasks/site.rake
@@ -1,8 +1,19 @@
 namespace :site do
-  desc "Harvest Neubloc job postings from pracuj.pl and store results in the database"
+  desc 'Harvest Neubloc job postings from pracuj.pl and store results in the database'
   task scrape_pracuj: :environment do
-  	jobcollector = JobCollector.new()
-  	jobcollector.collect
-  end	
+    log = ActiveSupport::Logger.new('log/scrape_pracuj.log')
+    start_time = Time.now
 
+    log.info "Task started at #{start_time}"
+    job_collector = JobCollector.new
+    job_collector.collect
+
+    jobs_count = Job.count
+    locations_count = Location.count
+    end_time = Time.now
+    duration = (start_time - end_time) / 1.minute
+    log.info "#{jobs_count}/#{locations_count} - Jobs/Locations total in db"
+    log.info "Task finished at #{end_time} and last #{duration} minutes."
+    log.close
+  end
 end

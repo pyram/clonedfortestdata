e5aa5ddb191cd246da32936d569b57d062d7cc22
pyram <pyram@tlen.pl>
2017-05-18 14:21:43 +0200

    corrections to layout in members photo margins added code from the template to schedule config

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,14 @@
+env :SHELL, '/usr/local/bin/zsh'
+env :PATH, '/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin'
+
+set :job_template, nil
+job_type :bundle, 'cd :path && bundle exec :task :output'
+job_type :command, 'cd :path && :task :output'
+
 every 1.hour do
   rake 'site:scrape_pracuj'
+end
+
+every :reboot do
+  command "bin/procodile"
 end
